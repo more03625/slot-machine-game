25994290e2407bd10078274692ea94c572e20e5d
RAHUL MORE <more03625@gmail.com>
2021-06-22 18:48:26 +0530

    slot machin game

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import SlotM from './components/Slotmachine/Slotmachine';
 
 const App = () => {
   return <>
-    <h1 className="app-heading"> 🎰 Welcome to Slot machine 🎰</h1>
     {/* We can use props here */}
+    <h1 className="app-heading"> 🎰 Welcome to Slot machine 🎰</h1>
+
     <div className="slot_machine">
-      <SlotM x='🎰' y='😄' z='🍪'/>
-      <SlotM x= ' 🍎 ' y=' 🍌 ' z=' 🥭 '/>
-      <SlotM x='😄' y='😄' z='😄'/>
+        <SlotM x='🎰' y='😄' z='🍪'/>
+        <SlotM x= ' 🍎 ' y=' 🍌 ' z=' 🥭 '/>
+        <SlotM x='😄' y='😄' z='😄'/>
     </div>
   </>
 }
